9b9d3039a5302409777e2fde05de31cdd39d3169
Rohit verma <28vrohit@gmail.com>
2022-08-05 17:15:54 +0530

    Update doctor.js

--- ui/src/data/doctor.js
+++ ui/src/data/doctor.js
@@ -16,7 +16,7 @@ export const doctors = [
         hospital: "abc",
         experience: "10",
         award: "xyz", 
-        img: "https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?w=740&t=st=1659632411~exp=1659633011~hmac=6b195d5242f240e034cc43aec1efaee2534c900761271be0728b951aafa8de63.jpg",           
+        img: "https://images.pexels.com/photos/2182979/pexels-photo-2182979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg",           
     },
     {
         id:3,
@@ -25,7 +25,7 @@ export const doctors = [
         hospital: "uvw",
         experience: "5",
         award: "abc",   
-        img: "https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?w=740&t=st=1659632411~exp=1659633011~hmac=6b195d5242f240e034cc43aec1efaee2534c900761271be0728b951aafa8de63.jpg",         
+        img: "https://images.pexels.com/photos/4167541/pexels-photo-4167541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg",         
     },
     {
         id:4,
@@ -34,6 +34,6 @@ export const doctors = [
         hospital: "abc",
         experience: "6",
         award: "rv",  
-        img: "https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?w=740&t=st=1659632411~exp=1659633011~hmac=6b195d5242f240e034cc43aec1efaee2534c900761271be0728b951aafa8de63.jpg",          
+        img: "https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=600.jpg",          
     },
 ]
